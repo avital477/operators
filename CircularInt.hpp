3e03a30a2ca4e72a249b5865bc4de9f4d947400d
avital477 <33714153+avital477@users.noreply.github.com>
2018-04-23 01:39:04 +0300

    Add files via upload

---
+++ CircularInt.hpp
@@ -0,0 +1,41 @@
+using namespace std;
+#pragma once
+#include <iostream>
+
+class CircularInt{
+public:
+    int start;
+    int end;
+    int current;
+
+     CircularInt (int s, int e){
+        start = s;
+        end = e;
+        current = s;
+    }
+    // CircularInt operator () (int x, int y);
+     CircularInt&  operator += (const int &b);
+     CircularInt& operator-=(const int& other);
+    CircularInt& operator*=(const int& mult);
+
+    int operator-();
+    int operator++(int);     
+     CircularInt& operator ++ ();
+     CircularInt&  operator + (const CircularInt &b);
+
+
+    
+    // CircularInt operator - (const CircularInt &b);
+    
+    // CircularInt& operator ++ ();
+    // CircularInt operator ++ (int);
+   // };
+   // ~CircularInt();
+
+
+};
+ ostream& operator <<(ostream& os, const CircularInt& b);
+ const CircularInt operator+(const int a,const CircularInt& b);
+  const CircularInt operator / (const CircularInt& a,const int& div);
+  const CircularInt operator - (const int a,const CircularInt& other);
+const CircularInt operator + (const CircularInt& a, const CircularInt& b);
